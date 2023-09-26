d1e477be3c098304f81a84dd18e239edda343643
neoisrecursive <neo.leijondahl@gmail.com>
2023-09-26 20:42:54 +0200

    Add comments and refactoring to config

--- config/backup.php
+++ config/backup.php
@@ -1,12 +1,51 @@
 <?php
 
 return [
+    /*
+    / The path to the content directory
+    / This is used by the default content backup driver
+    */
     'content_path' => storage_path('content'),
-    'backup' => [
+
+    /*
+    / The backup destination options
+    */
+    'destination' => [
         'disk' => 'local',
         'path' => 'statamic-backups',
-        'max_backups' => 10,
     ],
+
+    /*
+    / The path to the temp directory
+    */
+    'temp_path' => storage_path('temp/statamic-backups'),
+
+    /*
+    / The maximum number of backups to keep
+    / when superceded the oldest backup will be deleted
+    */
+    'max_backups' => 10,
+
+    /*
+    / The backup password
+    / set to null to disable password protection
+    */
+    'password' => env('BACKUP_PASSWORD', null),
+
+    /*
+    / The backup schedule options
+    / set to false to disable automatic backups
+    */
+    'schedule' => [
+        'frequency' => 'daily',
+        'time' => '00:00',
+    ],
+
+    /*
+    / The backup drivers to use
+    / These are the drivers that will be used to backup your site
+    / You can add your own drivers here
+    */
     'backup_drivers' => [
         Itiden\Backup\Drivers\Content::class,
         Itiden\Backup\Drivers\Assets::class,
