c48a9038a931fb19b59108ba3037836da0dfe5c0
neoisrecursive <neo.leijondahl@gmail.com>
2023-09-26 21:15:42 +0200

    Add phpdocs to contract

--- src/Contracts/BackupDriver.php
+++ src/Contracts/BackupDriver.php
@@ -8,9 +8,28 @@ use ZipArchive;
 
 interface BackupDriver
 {
+    /**
+     * Get the key of the driver.
+     * 
+     * @return string
+     */
     public static function getKey(): string;
 
+    /**
+     * Run the restore process.
+     * 
+     * @param ZipArchive $zip
+     * 
+     * @return void
+     */
     public function restore(string $path): void;
 
+    /**
+     * Run the backup process.
+     * 
+     * @param ZipArchive $zip
+     * 
+     * @return void
+     */
     public function backup(ZipArchive $zip): void;
 }
