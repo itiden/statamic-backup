54526c54b7ffbb8f43af384f888cfbbc2d3321fb
neoisrecursive <neo.leijondahl@gmail.com>
2023-09-28 11:35:10 +0200

    Add make most routes api routes

--- routes/cp.php
+++ routes/cp.php
@@ -2,24 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 use Itiden\Backup\Http\Controllers\Api\BackupController;
+use Itiden\Backup\Http\Controllers\Api\RestoreController;
 use Itiden\Backup\Http\Controllers\CreateBackupController;
 use Itiden\Backup\Http\Controllers\DownloadBackupController;
 use Itiden\Backup\Http\Middleware\CanCreateBackups;
 use Itiden\Backup\Http\Middleware\CanManageBackups;
+use Itiden\Backup\Http\Middleware\CanRestoreBackups;
 
 Route::name('itiden.backup.')->prefix('backups')->group(function () {
     Route::view('/', 'itiden-backup::backups')
         ->name('index');
+})->middleware([CanManageBackups::class]);
 
+Route::name('api.itiden.backup')->prefix('api/backups')->group(function () {
     Route::post('/create', CreateBackupController::class)
         ->middleware(CanCreateBackups::class)
         ->name('create');
 
-    Route::get('/download/{timestamp}', DownloadBackupController::class)
-        ->name('download');
-})->middleware([CanManageBackups::class]);
-
-Route::name('api.itiden.backup')->prefix('api/backups')->group(function () {
     Route::get('/', BackupController::class)
         ->name('index');
+
+    Route::get('/download/{timestamp}', DownloadBackupController::class)
+        ->name('download');
+
+    Route::post('/restore/{timestamp}', RestoreController::class)
+        ->middleware(CanRestoreBackups::class)
+        ->name('restore');
 })->middleware([CanManageBackups::class]);
