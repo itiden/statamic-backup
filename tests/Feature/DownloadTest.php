9cbbf1834f606a5409da6ed5a38e852506e5dc75
neoisrecursive <neo.leijondahl@gmail.com>
2023-09-29 10:15:01 +0200

    Add download backup tests

---
+++ tests/Feature/DownloadTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Itiden\Backup\Facades\Backuper;
+use Statamic\Facades\Role;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+use function Pest\Laravel\getJson;
+
+uses()->group('download');
+
+it('cant be downloaded by a guest', function () {
+    $backup = Backuper::backup();
+
+    $responseJson = getJson(cp_route('api.itiden.backup.download', $backup->timestamp));
+    $response = get(cp_route('api.itiden.backup.download', $backup->timestamp));
+
+    expect($responseJson->status())->toBe(401);
+    expect($response->status())->toBe(302);
+});
+
+it('cant be downloaded by a user without permissons a backup', function () {
+    $backup = Backuper::backup();
+
+    actingAs(user());
+
+    $responseJson = getJson(cp_route('api.itiden.backup.download', $backup->timestamp));
+    $response = get(cp_route('api.itiden.backup.download', $backup->timestamp));
+
+    expect($responseJson->status())->toBe(403);
+    expect($response->status())->toBe(302);
+});
+
+it('can be downloaded by a user with download backups permission', function () {
+    $backup = Backuper::backup();
+
+    $user = user();
+
+    $user->assignRole('admin')->save();
+
+    actingAs($user);
+
+    $response = get(cp_route('api.itiden.backup.download', $backup->timestamp));
+
+    expect($response)->assertDownload();
+});
