121ebf1e5b0d5434fe601ff73a931c122450bf5c
neoisrecursive <neo.leijondahl@gmail.com>
2023-10-02 13:12:05 +0200

    Add comments to describe methods

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -9,6 +9,9 @@ use Illuminate\Http\RedirectResponse;
 
 class Response
 {
+    /**
+     * Return a JSON response or redirect back with an error message and code.
+     */
     public static function error(string $message, int $code = 500): JsonResponse|RedirectResponse
     {
         if (request()->expectsJson()) {
@@ -20,6 +23,9 @@ class Response
         return redirect()->back()->with('error', $message);
     }
 
+    /**
+     * Return a JSON response or redirect back with a success message.
+     */
     public static function success(string $message): JsonResponse|RedirectResponse
     {
         if (request()->expectsJson()) {
