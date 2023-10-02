c9ba8f603327477618318f8c67352c91a44e9687
neoisrecursive <neo.leijondahl@gmail.com>
2023-10-02 10:18:23 +0200

    Add isLocal method for assets

--- src/Drivers/Assets.php
+++ src/Drivers/Assets.php
@@ -7,6 +7,7 @@ namespace Itiden\Backup\Drivers;
 use Illuminate\Support\Facades\File;
 use Itiden\Backup\Contracts\BackupDriver;
 use Itiden\Backup\Support\Zipper;
+use Statamic\Assets\AssetContainer as Container;
 use Statamic\Facades\AssetContainer;
 
 class Assets implements BackupDriver
@@ -18,16 +19,25 @@ class Assets implements BackupDriver
 
     public function restore(string $content): void
     {
-        AssetContainer::all()->each(function ($container) use ($content) {
-            File::cleanDirectory($container->diskPath());
-            File::copyDirectory("{$content}/{$container->handle()}", $container->diskPath());
-        });
+        AssetContainer::all()
+            ->filter([static::class, 'isLocal'])
+            ->each(function ($container) use ($content) {
+                File::cleanDirectory($container->diskPath());
+                File::copyDirectory("{$content}/{$container->handle()}", $container->diskPath());
+            });
     }
 
     public function backup(Zipper $zip): void
     {
-        AssetContainer::all()->each(function ($container) use ($zip) {
-            $zip->addDirectory($container->diskPath(), static::getKey() . '/' . $container->handle());
-        });
+        AssetContainer::all()
+            ->filter([static::class, 'isLocal'])
+            ->each(function ($container) use ($zip) {
+                $zip->addDirectory($container->diskPath(), static::getKey() . '/' . $container->handle());
+            });
+    }
+
+    public static function isLocal(Container $container): bool
+    {
+        return config('filesystems.disks.' . $container->diskHandle())['driver'] === 'local';
     }
 }
