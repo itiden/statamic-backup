82c1c5c7882734008fa3bcdea35fb4abce194160
neoisrecursive <neo.leijondahl@gmail.com>
2023-10-03 07:57:10 +0200

    Add necessary deps and uploader

--- resources/js/components/Backup.vue
+++ resources/js/components/Backup.vue
@@ -1,20 +1,58 @@
 <template>
-  <div>
-    <div class="flex justify-between">
-      <h1 class="mb-6">Backups</h1>
-      <backup-actions />
-    </div>
+  <uploader
+    :enabled="true"
+    @updated="uploadsUpdated"
+    :url="url"
+    @upload-complete="uploadCompleted"
+    @error="uploadError"
+  >
+    <div slot-scope="{ dragging }" class="min-h-screen">
+      <div class="drag-notification" v-show="dragging">
+        <svg-icon name="upload" class="h-12 w-12 m-4" />
+        <span>{{ __("Drop File to Upload") }}</span>
+      </div>
 
-    <backup-listing />
-  </div>
+      <div class="flex justify-between">
+        <h1 class="mb-6">Backups</h1>
+        <backup-actions />
+      </div>
+
+      <uploads v-if="uploads.length" :uploads="uploads" class="-mt-px" />
+
+      <backup-listing />
+    </div>
+  </uploader>
 </template>
 <script>
 import Listing from "./Listing.vue";
 import Actions from "./Actions.vue";
+import Uploader from "../../../vendor/statamic/cms/resources/js/components/assets/Uploader.vue";
+import Uploads from "../../../vendor/statamic/cms/resources/js/components/assets/Uploads.vue";
+
 export default {
   components: {
     "backup-listing": Listing,
     "backup-actions": Actions,
+    Uploader,
+    Uploads,
+  },
+  data() {
+    return {
+      uploads: [],
+      url: cp_url("/backups"),
+    };
+  },
+  methods: {
+    uploadsUpdated(uploads) {
+      this.uploads = uploads;
+    },
+    uploadCompleted(backup) {
+      this.$toast.success(backup.filename + " uploaded successfully.");
+    },
+    uploadError(error) {
+      console.log(error);
+      this.$toast.error(error);
+    },
   },
 };
 </script>
