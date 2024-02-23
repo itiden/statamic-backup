824302e690eb3ea56e590db4e88615c94ec5e720
neoisrecursive <neo.leijondahl@gmail.com>
2024-02-23 13:56:58 +0100

    Add tests for destroy controller

---
+++ tests/Feature/DeleteBackupTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Itiden\Backup\Facades\Backuper;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\deleteJson;
+
+it('cant be deleted by a guest', function () {
+    $backup = Backuper::backup();
+
+    $res = deleteJson(cp_route('api.itiden.backup.destroy', $backup->timestamp));
+
+    expect($res->status())->toBe(401);
+});
+
+it('cant be deleted by a user without delete permisson', function () {
+    $backup = Backuper::backup();
+
+    actingAs(user());
+
+    $res = deleteJson(cp_route('api.itiden.backup.destroy', $backup->timestamp));
+
+    expect($res->status())->toBe(403);
+});
+
+it('can be deleted by a user with delete backups permission', function () {
+    $backup = Backuper::backup();
+
+    $user = user();
+
+    $user->assignRole('super admin')->save();
+
+    actingAs($user);
+
+    $response = deleteJson(cp_route('api.itiden.backup.destroy', $backup->timestamp));
+
+    expect($response->status())->toBe(200);
+});
