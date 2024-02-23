7eef1df97261842a423fe6d9d1f6e1834b1e30a6
neoisrecursive <neo.leijondahl@gmail.com>
2024-02-23 13:53:52 +0100

    Remove removed methods from docbloc

--- src/Facades/Backuper.php
+++ src/Facades/Backuper.php
@@ -4,17 +4,12 @@ declare(strict_types=1);
 
 namespace Itiden\Backup\Facades;
 
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 use Itiden\Backup\Backuper as BackuperService;
 use Itiden\Backup\DataTransferObjects\BackupDto;
 
 /**
  * @method static BackupDto backup()
- * @method static Collection<BackupDto> getBackups()
- * @method static BackupDto getBackup(string $backupName)
- * @method static BackupDto deleteBackup(string $backupName)
- * @method static bool clearBackups()
  *
  * @see \Itiden\Backup\Backuper
  */
