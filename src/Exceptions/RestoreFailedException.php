86c0235a965dac086322ec06664c05e8f2c24ae8
neoisrecursive <neo.leijondahl@gmail.com>
2024-03-04 12:48:34 +0100

    Fix restore exception message

--- src/Exceptions/RestoreFailedException.php
+++ src/Exceptions/RestoreFailedException.php
@@ -13,6 +13,6 @@ class RestoreFailedException extends Exception
     public function __construct(
         public BackupDto $backup
     ) {
-        parent::__construct('Backup failed ' . Carbon::now()->format('Ymd'));
+        parent::__construct("Restore to backup {$this->backup->name} failed " . Carbon::now()->format('Ymd'));
     }
 }
