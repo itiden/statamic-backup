996b38f0fe22b82b1fc9f5f387fbeaf518a193bd
NeoIsRecursive <neo.leijondahl@gmail.com>
2024-06-07 16:56:33 +0200

    Change restore command to use laravel prompts

--- src/Console/Commands/RestoreCommand.php
+++ src/Console/Commands/RestoreCommand.php
@@ -6,15 +6,20 @@ namespace Itiden\Backup\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
+use Itiden\Backup\Contracts\Repositories\BackupRepository;
 use Itiden\Backup\DataTransferObjects\BackupDto;
 use Itiden\Backup\Facades\Restorer;
+
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\select;
+use function Laravel\Prompts\spin;
 
 /**
  * Restore content from a directory / backup
  */
 class RestoreCommand extends Command implements PromptsForMissingInput
 {
-    protected $signature = 'statamic:backup:restore {path} {--force}';
+    protected $signature = 'statamic:backup:restore {--path} {--force}';
 
     protected $description = 'Reset or restore content from a directory / backup';
 
@@ -25,10 +30,27 @@ class RestoreCommand extends Command implements PromptsForMissingInput
         ];
     }
 
-    public function handle()
+    public function handle(BackupRepository $repo)
     {
-        if ($this->option('force') || $this->confirm('Are you sure you want to restore your content?')) {
-            Restorer::restore(BackupDto::fromAbsolutePath($this->argument('path')));
+        /* @var BackupDto $backup */
+        $backup = match (true) {
+            $this->hasOption('path') => BackupDto::fromAbsolutePath($this->option('path')),
+            default => BackupDto::fromFile(select(
+                'Which backup do you want to restore to?',
+                $repo->all()->flatMap(
+                    fn (BackupDto $backup) => [$backup->path => $backup->path]
+                )
+            )),
+        };
+
+        if (
+            $this->option('force')
+            || confirm(
+                label: "Are you sure you want to restore your content?",
+                hint: "This will overwrite your current content with state from {$backup->created_at->format('Y-m-d H:i:s')}"
+            )
+        ) {
+            spin(fn () => Restorer::restore($backup), 'Restoring backup');
         }
     }
 }
