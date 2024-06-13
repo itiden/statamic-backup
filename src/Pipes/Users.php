dcd20c64097c8cf8672f685dea74eb077d8f6ab4
Cédric Couret <cedric@nxstar.ca>
2024-06-13 17:02:39 -0400

    Merge default config with user-defined config for User pipeline

--- src/Pipes/Users.php
+++ src/Pipes/Users.php
@@ -6,6 +6,7 @@ namespace Itiden\Backup\Pipes;
 
 use Closure;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use Itiden\Backup\Abstracts\BackupPipe;
 use Itiden\Backup\Support\Zipper;
 
@@ -18,7 +19,7 @@ class Users extends BackupPipe
 
     public function restore(string $restoringFromPath, Closure $next)
     {
-        $destination = config('statamic.stache.stores.users.directory');
+        $destination = $this->getConfig()['users']->directory();
         $users = $this->getDirectoryPath($restoringFromPath);
 
         File::copyDirectory($users, $destination);
@@ -28,8 +29,29 @@ class Users extends BackupPipe
 
     public function backup(Zipper $zip, Closure $next)
     {
-        $zip->addDirectory(config('statamic.stache.stores.users.directory'), static::getKey());
+        $zip->addDirectory($this->getConfig()['users']->directory(), static::getKey());
 
         return $next($zip);
+    }
+
+    private function getConfig()
+    {
+        $config = require base_path('vendor').'/statamic/cms/config/stache.php';
+        $published = config('statamic.stache.stores');
+
+        $nativeStores = collect($config['stores'])
+            ->reject(fn ($config, $key) => $key === 'users' && config('statamic.users.repository') !== 'file')
+            ->map(function ($config, $key) use ($published) {
+                return array_merge($config, $published[$key] ?? []);
+            });
+
+        // Merge in any user defined stores that aren't native.
+        $stores = $nativeStores->merge(collect($published)->diffKeys($nativeStores));
+
+        $stores = $stores->map(function ($config) {
+            return app($config['class'])->directory($config['directory'] ?? null);
+        });
+
+        return $stores->all();
     }
 }
