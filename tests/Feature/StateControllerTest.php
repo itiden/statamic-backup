a74443dc436483f64390c319e2f6dbc56c14555a
NeoIsRecursive <neo.leijondahl@gmail.com>
2025-01-16 13:12:27 +0100

    Add tests for state controller

---
+++ tests/Feature/StateControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Itiden\Backup\Http\Controllers\Api\StateController;
+use Itiden\Backup\State;
+use Itiden\Backup\StateManager;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\getJson;
+
+covers(StateController::class);
+
+describe('api:state', function () {
+    beforeEach(fn () => app(StateManager::class)->setState(State::Idle));
+
+    it('returns the correct state', function () {
+        $user = user();
+
+        $user->set('roles', ['admin'])->save();
+
+        actingAs($user);
+
+        getJson(cp_route('api.itiden.backup.state'))
+            ->assertOk()
+            ->assertExactJson([
+                'state' => 'idle'
+            ]);
+    });
+});
