3b8fb6fa2c1339a3880dae3f322ad21e2f3c04a6
NeoIsRecursive <neo.leijondahl@gmail.com>
2025-03-27 13:39:48 +0100

    Add better test for restore

--- tests/Unit/RestorerTest.php
+++ tests/Unit/RestorerTest.php
@@ -9,8 +9,10 @@ use Itiden\Backup\Facades\Backuper;
 use Itiden\Backup\Facades\Restorer;
 use Itiden\Backup\Enums\State;
 use Itiden\Backup\StateManager;
+use Statamic\Facades\Stache;
 
 use function Itiden\Backup\Tests\fixtures_path;
+use function Itiden\Backup\Tests\user;
 
 describe('restorer', function (): void {
     it('can restore from timestamp', function (): void {
@@ -23,6 +25,32 @@ describe('restorer', function (): void {
         Restorer::restoreFromTimestamp($backup->timestamp);
 
         expect(File::isEmptyDirectory(fixtures_path('content/collections')))->toBeFalse();
+    });
+
+    it('restores correct files', function (): void {
+        user();
+        expect(File::allFiles(Stache::store('entries')->directory()))->toHaveCount(2); // 1 entry, 1 collection
+        expect(File::allFiles(Stache::store('form-submissions')->directory()))->toHaveCount(1);
+        expect(File::allFiles(Stache::store('users')->directory()))->toHaveCount(1);
+
+        // config()->set('backup.stache_stores', [
+        //     'form-submissions',
+        // ]);
+
+        $backup = Backuper::backup();
+
+        File::cleanDirectory(fixtures_path('content')); // Simulate the stache directories doesnt exist anymore by removing the their parent directory
+        File::cleanDirectory(Stache::store('users')->directory());
+
+        expect(file_exists(Stache::store('entries')->directory()))->toBeFalse();
+        expect(file_exists(Stache::store('form-submissions')->directory()))->toBeFalse();
+        expect(File::allFiles(Stache::store('users')->directory()))->toHaveCount(0);
+
+        Restorer::restore($backup);
+
+        expect(File::allFiles(Stache::store('entries')->directory()))->toHaveCount(2); // 1 entry, 1 collection
+        expect(File::allFiles(Stache::store('form-submissions')->directory()))->toHaveCount(1);
+        expect(File::allFiles(Stache::store('users')->directory()))->toHaveCount(1);
     });
 
     it('throws an exception if the backup path does not exist', function (): void {
