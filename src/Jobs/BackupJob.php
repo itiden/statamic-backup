a2f5647feee0bb366f9695f1f87aa84dfe573fbd
NeoIsRecursive <neo.leijondahl@gmail.com>
2025-05-19 13:06:57 +0200

    fix: remove use of laravel 11 specific trait to keep supporting laravel 10
    Fixes #67

--- src/Jobs/BackupJob.php
+++ src/Jobs/BackupJob.php
@@ -4,16 +4,23 @@ declare(strict_types=1);
 
 namespace Itiden\Backup\Jobs;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Cache\Repository;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Itiden\Backup\Backuper;
 use Itiden\Backup\StateManager;
 
 final class BackupJob implements ShouldQueue
 {
+    // TODO(@neoisrecursive): Refactor this to use the `Illuminate\Foundation\Queue\Queueable` trait when dropping support for Laravel 10.
     use Queueable;
+    use Dispatchable;
+    use SerializesModels;
+    use InteractsWithQueue;
 
     /**
      * Create a new job instance.
