c7ee4d79ca450a2688500f35740dcc91d6662d92
NeoIsRecursive <neo.leijondahl@gmail.com>
2025-08-26 11:57:56 +0200

    wip

--- client/src/components/Backup.vue
+++ client/src/components/Backup.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { Listing, DropdownItem, Header, Button } from "@statamic/cms/ui";
-import { requireElevatedSession } from "@statamic/cms"
+import { requireElevatedSession } from "@statamic/cms";
 import { useBackupStore } from "../store";
 import { ref, watch } from "vue";
 import { useResumable } from "../resumable";
 
-const props = defineProps(['chunkSize']);
+const props = defineProps(["chunkSize"]);
 
 const backupStore = useBackupStore();
 
@@ -14,102 +14,144 @@ const listing = ref(null);
 const dropZone = ref(null);
 const browseTarget = ref(null);
 
-const { files } = useResumable({ chunkSize: props.chunkSize ?? 2 * 1024 * 1024, dropZone, browseTarget, onFileUploaded: (file) => {
-  listing.value.refresh();
-}});
+const { files } = useResumable({
+  chunkSize: props.chunkSize ?? 2 * 1024 * 1024,
+  dropZone,
+  browseTarget,
+  onFileUploaded: (file) => {
+    listing.value.refresh();
+  },
+});
 
 backupStore.startPolling();
 
-watch(() => backupStore.status, (newStatus, oldStatus) => {
+watch(
+  () => backupStore.status,
+  (newStatus, oldStatus) => {
+    if (oldStatus === "initializing") return;
     if (newStatus !== oldStatus) {
-        listing.value.refresh();
+      listing.value.refresh();
     }
+  }
+);
+
+const withErrHandling = (fn) => async (params) => {
+  return fn(params).catch((err) => {
+    console.error(err);
+
+    if (err.response) {
+      Statamic.$toast.error(err.response.data.message);
+    } else {
+      Statamic.$toast.error(err.message);
+    }
+  });
+};
+
+const queueRestore = withErrHandling(async (id) => {
+  await requireElevatedSession();
+
+  const { data } =
+    await window.Statamic.$app.config.globalProperties.$axios.post(
+      cp_url(`api/backups/restore/${id}`)
+    );
+
+  Statamic.$toast.info(__(data.message));
 });
 
+const queueBackup = withErrHandling(async () => {
+  backupStore.setStatus("backup_in_progress");
 
-const restoreFrom = async (id) => {
+  const { data } =
+    await window.Statamic.$app.config.globalProperties.$axios.post(
+      cp_url("api/backups")
+    );
 
-    try {
-        await requireElevatedSession();
+  Statamic.$toast.info(__(data.message));
+  listing.value.refresh();
+});
 
-        const { data } = await window.Statamic.$app.config.globalProperties.$axios.post(cp_url(`api/backups/restore/${id}`));
+const deleteBackup = withErrHandling(async (id) => {
+  await requireElevatedSession();
 
-        Statamic.$toast.info(__(data.message));
-    } catch (e) {
-        console.error(e);
+  const { data } =
+    await window.Statamic.$app.config.globalProperties.$axios.delete(
+      cp_url(`api/backups/${id}`)
+    );
 
-        if (error.response.data.message) {
-            Statamic.$toast.error(message);
-        } else {
-            Statamic.$toast.error(__('statamic-backup::backup.restore.failed'));
-        }
-    }
-}
-
-const queueBackup = async () => {
-    try {
-        backupStore.setStatus('backup_in_progress');
-
-        const { data } = await window.Statamic.$app.config.globalProperties.$axios.post(cp_url("api/backups"));
-
-        Statamic.$toast.info(__(data.message));
-        listing.value.refresh();
-    } catch (e) {
-        console.error(e);
-
-        if (error.response.data.message) {
-            Statamic.$toast.error(message);
-        } else {
-            Statamic.$toast.error(__('statamic-backup::backup.restore.failed'));
-        }
-    }
-}
-
-const deleteBackup = async (id) => {
-    try {
-        await requireElevatedSession();
-
-        const { data } = await window.Statamic.$app.config.globalProperties.$axios.delete(cp_url(`api/backups/${id}`));
-
-        Statamic.$toast.info(__(data.message));
-        listing.value.refresh();
-    } catch (e) {
-        console.error(e);
-
-        if (error.response.data.message) {
-            Statamic.$toast.error(message);
-        } else {
-            Statamic.$toast.error(__('statamic-backup::backup.destroy.failed'));
-        }
-    }
-}
-
+  Statamic.$toast.info(__(data.message));
+  listing.value.refresh();
+});
 </script>
 
 <template>
-    <Header :icon="database" :title="__('statamic-backup::backup.title')">
-        <Button variant="subtle" ref="browseTarget">{{ __("statamic-backup::backup.upload.label") }}</Button>
-        <Button variant="primary" v-on:click="queueBackup"
-            :disabled="!backupStore.abilities.backup.isPossible">{{ __("statamic-backup::backup.create") }}</Button>
-    </Header>
+  <Header :icon="database" :title="__('statamic-backup::backup.title')">
+    <Button variant="subtle" ref="browseTarget">{{
+      __("statamic-backup::backup.upload.label")
+    }}</Button>
+    <Button
+      variant="primary"
+      v-on:click="queueBackup"
+      :disabled="!backupStore.abilities.backup.isPossible"
+      :loading="backupStore.status === 'backup_in_progress'"
+    >
+      {{ __("statamic-backup::backup.create") }}
+    </Button>
+  </Header>
 
-    <p v-for="file in files" :key="file.file.uniqueIdentifier" class="mb-2">
-        <span>{{ file.file.fileName }}</span>
-        <span v-if="file.status === 'uploading'"> - {{ Math.round(file.progress * 100) }}%</span>
-        <span v-if="file.status === 'retrying'"> - {{ __('statamic-backup::backup.upload.retrying') }}</span>
-        <span v-if="file.status === 'error'" class="text-red-600"> - {{ __('statamic-backup::backup.upload.error') }}</span>
-    </p>
+  <p v-for="file in files" :key="file.file.uniqueIdentifier" class="mb-2">
+    <span>{{ file.file.fileName }}</span>
+    <span v-if="file.status === 'uploading'">
+      - {{ Math.round(file.progress * 100) }}%</span
+    >
+    <span v-if="file.status === 'retrying'">
+      - {{ __("statamic-backup::backup.upload.retrying") }}</span
+    >
+    <span v-if="file.status === 'error'" class="text-red-600">
+      - {{ __("statamic-backup::backup.upload.error") }}</span
+    >
+  </p>
 
-    <Listing ref="listing" :allowSearch="false" :allowCustomizingColumns="false" :url="cp_url('api/backups')">
-        <template #prepended-row-actions="{ row }">
-            <DropdownItem v-if="backupStore.abilities.download.isPermitted"
-                :text="__('statamic-backup::backup.download.label')"
-                :href="`${cp_url('api/backups/download')}/${row.id}`" />
-            <DropdownItem v-if="backupStore.abilities.restore.isPermitted"
-                :disabled="!backupStore.abilities.restore.isPossible" @click="restoreFrom(row.id)"
-                :text="__('statamic-backup::backup.restore.label')" />
-            <DropdownItem v-if="backupStore.abilities.destroy.isPermitted" variant="destructive"
-                @click="deleteBackup(row.id)" :text="__('statamic-backup::backup.destroy.label')" />
-        </template>
-    </Listing>
+  <Listing
+    ref="listing"
+    :allowSearch="false"
+    :allowCustomizingColumns="false"
+    :url="cp_url('api/backups')"
+    :columns="[
+      {
+        field: 'name',
+        label: __('statamic-backup::backup.name'),
+        visible: true,
+      },
+      {
+        field: 'created_at',
+        label: __('statamic-backup::backup.created_at'),
+        visible: true,
+      },
+      {
+        field: 'size',
+        label: __('statamic-backup::backup.size'),
+        visible: true,
+      },
+    ]"
+  >
+    <template #prepended-row-actions="{ row }">
+      <DropdownItem
+        v-if="backupStore.abilities.download.isPermitted"
+        :text="__('statamic-backup::backup.download.label')"
+        :href="`${cp_url('api/backups/download')}/${row.id}`"
+      />
+      <DropdownItem
+        v-if="backupStore.abilities.restore.isPermitted"
+        :disabled="!backupStore.abilities.restore.isPossible"
+        v-on:click="queueRestore(row.id)"
+        :text="__('statamic-backup::backup.restore.label')"
+      />
+      <DropdownItem
+        v-if="backupStore.abilities.destroy.isPermitted"
+        variant="destructive"
+        v-on:click="deleteBackup(row.id)"
+        :text="__('statamic-backup::backup.destroy.label')"
+      />
+    </template>
+  </Listing>
 </template>
