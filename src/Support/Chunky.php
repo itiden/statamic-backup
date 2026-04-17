8862cc221773559061ac0221c0d5f3faea7af23c
NeoIsRecursive <neo.leijondahl@gmail.com>
2026-04-17 09:22:11 +0200

    fix

--- src/Support/Chunky.php
+++ src/Support/Chunky.php
@@ -38,8 +38,8 @@ final class Chunky
 
     /**
      * Store a chunk of a file. If all chunks are uploaded, merge them into a single file.
-     * @param $dto ChunkyUploadDto
-     * @param ?closure(string) $onCompleted Callback to run when the file is fully uploaded.
+     *
+     * @param ?Closure(string):void $onCompleted Callback to run when the file is fully uploaded.
      */
     public function put(ChunkyUploadDto $dto, ?Closure $onCompleted = null): JsonResponse
     {
