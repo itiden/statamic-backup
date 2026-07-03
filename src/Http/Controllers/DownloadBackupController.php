c5e73835fabf19e6f189d88fb869b7f6eaf2710c
Andreas Berqvist <andreas@itiden.se>
2026-07-03 20:24:00 +0200

    Fix download

--- src/Http/Controllers/DownloadBackupController.php
+++ src/Http/Controllers/DownloadBackupController.php
@@ -9,11 +9,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Storage;
 use Itiden\Backup\Contracts\Repositories\BackupRepository;
-use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 final readonly class DownloadBackupController
 {
-    public function __invoke(Request $request, string $id, BackupRepository $repo): StreamedResponse
+    public function __invoke(Request $request, string $id, BackupRepository $repo): Response
     {
         $backup = $repo->find($id);
 
@@ -26,6 +26,22 @@ final readonly class DownloadBackupController
 
         $backup->getMetadata()->addDownload($user);
 
-        return Storage::disk(Config::string('backup.destination.disk'))->download($backup->path);
+        if (function_exists('set_time_limit')) {
+            set_time_limit(0);
+        }
+
+        // Clean and close all active output buffers to allow streaming without running out of memory
+        while (ob_get_level() > 0) {
+            ob_end_clean();
+        }
+
+        $disk = Storage::disk(Config::string('backup.destination.disk'));
+
+        try {
+            $path = $disk->path($backup->path);
+            return response()->download($path);
+        } catch (\Throwable) {
+            return $disk->download($backup->path);
+        }
     }
 }
